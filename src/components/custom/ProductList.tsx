2f6b156d0d7c30a7aeaa91df23cb927bfde8db50
codewithzain099 <zainiikhan099@gmail.com>
2024-10-05 06:29:19 -0700

    temp changed two server compoent to static

--- src/components/custom/ProductList.tsx
+++ src/components/custom/ProductList.tsx
@@ -1,23 +1,58 @@
 /** @format */
 
+// /** @format */
+
+// import React from 'react';
+// import SingleMainProduct from './SingleMainProduct';
+// import { ProductInterface } from '@/types/productInterface';
+
+// const ProductList = async () => {
+//   try {
+//     const response = await fetch(`http://localhost:3000/api/product`, {
+//       method: 'GET',
+//       cache: 'reload',
+//     });
+
+//     // Check if the response is okay (status 200-299)
+//     if (!response || !response.status) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     const { products } = data;
+
+//     // Check if products are available
+//     if (!products) {
+//       console.error('No products found');
+//       return <p>No products available</p>; // Render a message when no products are found
+//     }
+
+//     return (
+//       <div className='flex flex-row gap-4 items-center flex-wrap justify-center p-4'>
+//         {products.map((item: ProductInterface, index: number) => (
+//           <div key={index}>
+//             <SingleMainProduct product={item} />
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   } catch (error) {
+//     console.error('Failed to fetch products:', error);
+//     return <p>Error fetching products. Please try again later.</p>; // Render a message when there's an error
+//   }
+// };
+
+// export default ProductList;
+
 import React from 'react';
-import SingleMainProduct from './SingleMainProduct';
-import { ProductInterface } from '@/types/productInterface';
 
 const ProductList = async () => {
-  const response = fetch(`http://localhost:3000/api/product`, {
-    method: 'GET',
-    cache: 'reload',
-  });
-  const data = await (await response).json();
-  const { products } = data;
   return (
-    <div className='flex flex-row gap-4  items-center  flex-wrap justify-center p-4 '>
-      {products?.map((item: ProductInterface, index: number) => (
-        <div key={index}>
-          <SingleMainProduct product={item} />
-        </div>
-      ))}
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab deserunt
+      nostrum magni quasi similique accusantium facere inventore hic vitae nisi
+      culpa, necessitatibus obcaecati ullam reiciendis dicta fugiat dignissimos
+      officiis laboriosam?
     </div>
   );
 };
--- src/components/custom/TrustedCompanies.tsx
+++ src/components/custom/TrustedCompanies.tsx
@@ -1,101 +1,119 @@
 /** @format */
-import { cn } from '@/lib/utils';
-import Marquee from '../ui/marquee';
-import Image from 'next/image';
-import { CategoryInterface } from '@/types/categoryInterface';
 
-const ReviewCard = ({
-  name,
-  description = 'No description available', // Fallback if description is undefined
-  image_url = '/default-image.jpg', // Fallback if image_url is undefined
-}: {
-  name: string;
-  description?: string; // Make description optional
-  image_url?: string; // Make image_url optional
-}) => {
+// /** @format */
+// import { cn } from '@/lib/utils';
+// import Marquee from '../ui/marquee';
+// import Image from 'next/image';
+// import { CategoryInterface } from '@/types/categoryInterface';
+
+// const ReviewCard = ({
+//   name,
+//   description = 'No description available', // Fallback if description is undefined
+//   image_url = '/default-image.jpg', // Fallback if image_url is undefined
+// }: {
+//   name: string;
+//   description?: string; // Make description optional
+//   image_url?: string; // Make image_url optional
+// }) => {
+//   return (
+//     <figure
+//       className={cn(
+//         'relative w-64 cursor-pointer overflow-hidden rounded-xl border p-2 flex flex-col justify-center',
+//         'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]', // light styles
+//         'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]' // dark styles
+//       )}
+//     >
+//       <div className='flex flex-row items-center gap-2'>
+//         <Image
+//           className='rounded-full'
+//           width={45} // Changed to number type
+//           height={46} // Changed to number type
+//           alt={name}
+//           src={image_url} // Default image if image_url is missing
+//         />
+//         <div className='flex flex-col'>
+//           <figcaption className='text-sm font-medium dark:text-white'>
+//             {name}
+//           </figcaption>
+//           <p className='text-xs font-medium dark:text-white/40'>
+//             {description}
+//           </p>
+//         </div>
+//       </div>
+//     </figure>
+//   );
+// };
+
+// const TrustedCompanies = async () => {
+//   // Fetch categories data directly in the server component
+//   const response = await fetch(`http://localhost:3000/api/category`, {
+//     method: 'GET',
+//     cache: 'no-cache', // You may adjust this based on your caching strategy
+//   });
+
+//   if (!response.ok) {
+//     console.error('Failed to fetch data:', response.statusText);
+//     return (
+//       <div className='text-center text-red-500'>
+//         Error loading categories. Please try again later.
+//       </div>
+//     ); // Display an error message if the fetch fails
+//   }
+
+//   const { categories }: { categories: CategoryInterface[] } =
+//     await response.json();
+
+//   const halfLength = Math.ceil(categories.length / 2); // Use ceil for odd-length arrays
+//   const firstRow = categories.slice(0, halfLength);
+//   const secondRow = categories.slice(halfLength);
+
+//   return (
+//     <div className='relative flex w-full flex-col items-center justify-center overflow-hidden'>
+//       <Marquee
+//         pauseOnHover
+//         className='[--duration:20s]'
+//       >
+//         {firstRow.map((category) => (
+//           <ReviewCard
+//             key={category?.id}
+//             name={category.name}
+//             description={category.description}
+//             image_url={category.image_url}
+//           />
+//         ))}
+//       </Marquee>
+//       <Marquee
+//         reverse
+//         pauseOnHover
+//         className='[--duration:20s]'
+//       >
+//         {secondRow.map((category) => (
+//           <ReviewCard
+//             key={category?.id}
+//             name={category.name}
+//             description={category.description}
+//             image_url={category.image_url}
+//           />
+//         ))}
+//       </Marquee>
+//       <div className='pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background'></div>
+//       <div className='pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background'></div>
+//     </div>
+//   );
+// };
+
+// export default TrustedCompanies;
+
+import React from 'react';
+
+const TrustedCompanies = async () => {
   return (
-    <figure
-      className={cn(
-        'relative w-64 cursor-pointer overflow-hidden rounded-xl border p-2 justify-center  flex flex-col',
-        // light styles
-        'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
-        // dark styles
-        'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
-      )}
-    >
-      <div className='flex flex-row items-center gap-2'>
-        <Image
-          className='rounded-full'
-          width='45'
-          height='46'
-          alt={name}
-          src={image_url} // Default image if image_url is missing
-        />
-        <div className='flex flex-col'>
-          <figcaption className='text-sm font-medium dark:text-white'>
-            {name}
-          </figcaption>
-          <p className='text-xs font-medium dark:text-white/40'>
-            {description}
-          </p>
-        </div>
-      </div>
-    </figure>
-  );
-};
-
-async function TrustedCompanies() {
-  const response = await fetch(`http://localhost:3000/api/category`, {
-    method: 'GET',
-    cache: 'no-cache',
-  }).catch(() => null);
-
-  if (!response) {
-    // Handle the error, like returning an empty state or logging
-    console.error('Failed to fetch data');
-    return;
-  }
-
-  const data = await response.json();
-  const { categories } = data;
-
-  const firstRow = categories.slice(0, categories.length / 2);
-  const secondRow = categories.slice(categories.length / 2);
-
-  return (
-    <div className='relative flex w-full flex-col items-center justify-center overflow-hidden'>
-      {/* <h1 className='text-3xl font-bold'>Categories</h1> */}
-      <Marquee
-        pauseOnHover
-        className='[--duration:20s]'
-      >
-        {firstRow.map((category: CategoryInterface) => (
-          <ReviewCard
-            key={category?.id}
-            name={category.name}
-            description={category.description}
-            image_url={category.image_url}
-          />
-        ))}
-      </Marquee>
-      <Marquee
-        reverse
-        pauseOnHover
-        className='[--duration:20s]'
-      >
-        {secondRow.map((category: CategoryInterface) => (
-          <ReviewCard
-            key={category?.id}
-            name={category.name}
-            description={category.description}
-            image_url={category.image_url}
-          />
-        ))}
-      </Marquee>
-      <div className='pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background'></div>
-      <div className='pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background'></div>
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo nesciunt
+      magni odit illum labore eligendi ipsa? Eaque quisquam harum a labore.
+      Labore error amet numquam saepe facere iste, unde eos.
     </div>
   );
-}
+};
 
 export default TrustedCompanies;
