f953422f229c50ea208ada591f8abe7e191befa3
codewithzain099 <zainiikhan099@gmail.com>
2024-10-07 12:29:05 -0700

    updated the db url i mean give it static

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,9 @@ export default defineConfig({
   schema: './src/models',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL! || '',
+    url:
+      process.env.DATABASE_URL! ||
+      'postgresql://neondb_owner:zdZ9qE3kWaUm@ep-damp-poetry-a7ofxgk8.ap-southeast-2.aws.neon.tech/euroenterprises?sslmode=require',
   },
   verbose: true,
   strict: true,
--- src/config/db.ts
+++ src/config/db.ts
@@ -3,5 +3,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(
+  process.env.DATABASE_URL! ||
+    'postgresql://neondb_owner:zdZ9qE3kWaUm@ep-damp-poetry-a7ofxgk8.ap-southeast-2.aws.neon.tech/euroenterprises?sslmode=require'
+);
 export const db = drizzle(sql);
